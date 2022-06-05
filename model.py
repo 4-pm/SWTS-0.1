18086f9e2c39ca582ce0e77f9830a61626f8a293
verdenkaa <nikitapahomov97@gmail.com>
2022-06-05 09:44:12 +0300

    realtime detection

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-# USAGE
-# based on this code https://proglib.io/p/real-time-object-detection/
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
@@ -7,70 +5,59 @@ import numpy as np
 import imutils
 import time
 import cv2
+import os
 
-prototxt="graph.pbtxt"
-model="model/model1.pth"
-min_confidence = 0.5
-
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["paper", "nopaper"]
-COLORS = [(40,50,60),((140,55,130)),(240,150,25)]
-
+path=os.path.join(os.path.abspath(os.curdir) , 'my_model.onnx')
+args_confidence = 0.2
+# initialize the list of class labels 
+CLASSES = ["none", 'paper', 'nopaper']
 # load our serialized model from disk
 print("[INFO] loading model...")
-
-net =cv2.dnn.readNetFromTensorflow(model,prototxt)
-
-# initialize the video stream, allow the cammera sensor to warmup,
+net = cv2.dnn.readNetFromONNX("C:/Users/User/Rover-Trasher/models/model.onnx")
+# initialize the video stream, allow the c
+#cammera sensor to warmup,
 # and initialize the FPS counter
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(0.5)
+#print("[INFO] starting video stream...")
+vs = VideoStream(src=1).start()
+time.sleep(2.0)
 fps = FPS().start()
+frame = vs.read()
+frame = imutils.resize(frame, width=400)
 
-# loop over the frames from the video stream
+
 while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=300)
+	frame = imutils.resize(frame, width=400)
 
 	# grab the frame dimensions and convert it to a blob
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True)
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (32, 32)),scalefactor=1.0/32
+                              , size=(32, 32), mean= (128,128,128), swapRB=True)
+	cv2.imshow("Cropped image", cv2.resize(frame, (32, 32)))
 
 	# pass the blob through the network and obtain the detections and
 	# predictions
 	net.setInput(blob)
 	detections = net.forward()
-
+	a = list(zip(CLASSES, detections[0]))
+    #print(a[max(a, key=lambda x: x[1])])
 	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
+
 		# extract the confidence (i.e., probability) associated with
 		# the prediction
-		print (detections)
-		confidence = detections[0, 0, i, 2]
-
-		if confidence > min_confidence:
-			# extract the index of the class label from the
-			# `detections`, then compute the (x, y)-coordinates of
-			# the bounding box for the object
-			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-
-			# draw the prediction on the frame
-			label = "{}: {:.2f}%".format(CLASSES[idx],
-				confidence * 100)
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y+3),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 1)
+	confidence = abs(detections[0][0]-detections[0][1])
+	#print("confidence = ", confidence)
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+	if (confidence > args_confidence) :
+		
+		class_mark=np.argmax(detections)
+		cv2.putText(frame, CLASSES[class_mark], (30,30),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (242, 230, 220), 2)
 
 	# show the output frame
-	cv2.imshow("Frame", frame)
+	cv2.imshow("Web camera view", frame)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key was pressed, break from the loop
@@ -79,12 +66,10 @@ while True:
 
 	# update the FPS counter
 	fps.update()
+	print(max(a, key=lambda x: x[1]))
 
 # stop the timer and display FPS information
 fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
