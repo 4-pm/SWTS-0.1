fbfc5d3c3e12043c83e6630ad10782d23e7e1731
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2022-10-18 12:48:44 +0300

    Update bluetooth_setup.ino

--- Arduino_code/sketch_oct13a/bluetooth_setup/bluetooth_setup.ino
+++ Arduino_code/sketch_oct13a/bluetooth_setup/bluetooth_setup.ino
@@ -1,9 +1,24 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3);
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2, INPUT);
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  mySerial.begin(38400);
+  Serial.println("start prg");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (mySerial.available()){
+    char c = mySerial.read();
+    Serial.print(c);
+  }
+  if (Serial.available()){
+    char c = Serial.read();
+    mySerial.write(c);
+  }
 }
